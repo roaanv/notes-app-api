62d70f76a9dd8b1386f85218c46596e2c25b82d3
Roaan Vos <rvos@atlassian.com>
2020-03-29 21:41:24 +1100

    Create DDB lib

--- src/create.ts
+++ src/create.ts
@@ -1,9 +1,7 @@
-import {DynamoDB} from "aws-sdk";
 import {Context, Handler} from "aws-lambda";
 import { v4 as uuidv4 } from 'uuid';
-import {ApiResponse, failure, success} from "./response-lib";
-
-const ddb = new DynamoDB.DocumentClient();
+import {ApiResponse, failure, success} from "./libs/response-lib";
+import {ddb} from "./libs/ddb-lib";
 
 export const handler:Handler<ApiResponse> = async (evt: any, ctx: Context): Promise<ApiResponse> => {
   const data = JSON.parse(evt.body);
---
+++ src/libs/ddb-lib.ts
@@ -0,0 +1,4 @@
+import {DynamoDB} from "aws-sdk";
+
+export const ddb = new DynamoDB.DocumentClient();
+
